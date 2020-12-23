def2b8f294980dc10c184ffdc46a736ea6fb5a94
gaoyongxin <1026265758@qq.com>
2020-12-23 14:21:42 +0800

    chore:better

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import index from '@/components/index.vue'
 
-describe('HelloWorld.vue', () => {
+describe('index.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const msg = 'index page'
+    const wrapper = shallowMount(index, {
       propsData: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
